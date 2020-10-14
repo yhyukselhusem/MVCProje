74bd8c675f9c4ac2e72f13f0da9e4280473ff5ed
yuksel hüsem <yhyukselhusem@outlook.com>
2020-10-15 01:17:38 +0300

    Order Service eklendi

---
+++ MVCProject/SERVICE/Repository/OrderService.cs
@@ -0,0 +1,14 @@
+﻿using MODEL.Entities;
+using SERVICE.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICE.Repository
+{
+   public class OrderService : BaseService<Order>
+    {
+    }
+}
